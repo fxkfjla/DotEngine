623fc128187649ec2eb0f8a6122b74137ed59167
fxkfjla <kacper.skr11@gmail.com>
2022-07-01 14:57:55 +0200

    message callback for validation layers added

--- src/Graphics.h
+++ src/Graphics.h
@@ -11,14 +11,32 @@ public:
     ~Graphics();
 private:
     void initVulkan();
+    std::vector<const char*> getRequiredExtensions() noexcept;
+
+    bool validationLayersSupported() noexcept;
+    void initDebugMessenger();
+    VkResult createDebugMessenger
+    (
+        VkInstance,
+        const VkDebugUtilsMessengerCreateInfoEXT*,
+        const VkAllocationCallbacks*,
+        VkDebugUtilsMessengerEXT*
+    ) noexcept;
+    void destroyDebugMessenger
+    (
+        VkInstance,
+        VkDebugUtilsMessengerEXT,
+        const VkAllocationCallbacks*
+    ) noexcept;
+    void setDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT&) noexcept;
 private:
     VkInstance vkInst;
 
+    VkDebugUtilsMessengerEXT debugMessenger;
     const std::vector<const char*> validationLayers =
     {
         "VK_LAYER_KHRONOS_validation"
     };
-    bool validationLayersSupported() noexcept;
     #ifdef NDEBUG
         const bool validationLayersEnabled = false;
     #else
