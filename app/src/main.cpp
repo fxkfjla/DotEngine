b7ba94a1dadb8c2a718c0d3e886448f32f38e870
fxkfjla <kacper.skr11@gmail.com>
2022-08-08 00:15:39 +0200

    catch dot exception

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,15 +1,20 @@
 #include "DotEngine.h"
+#include "DotException.h"
 
 #include <iostream>
 
 int main()
 {
     Window wnd;
-    DotEngine engine(wnd);
+    Dot::Engine engine(wnd);
 
     try
     {
         engine.run();
+    }
+    catch(const Dot::RuntimeError& e)
+    {
+        std::cerr << e.what() << '\n';
     }
     catch(const std::exception& e)
     {
