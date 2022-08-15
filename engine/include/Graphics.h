ce6d49b7e338d55dc354d24d51c959e09fa646e3
fxkfjla <kacper.skr11@gmail.com>
2022-08-15 22:24:13 +0200

    new graphics

--- engine/include/Graphics.h
+++ engine/include/Graphics.h
@@ -1,116 +1,23 @@
 #pragma once
 
 #include "dot_Vulkan.h"
-#include "Window.h"
+#include "dot_Instance.h"
 
-#include <vector>
-#include <optional>
+#include "Window.h"
 
 class Graphics
 {
 public:
     Graphics(Window&);
-    ~Graphics();
+    Graphics(const Graphics&) = delete;
+    Graphics(const Graphics&&) = delete;
+    Graphics& operator=(const Graphics&) = delete;
+    Graphics& operator=(const Graphics&&) = delete;
+
     void beginFrame();
     void endFrame();
 private:
-    void initVulkan();
-    std::vector<const char*> getRequiredExtensions() const noexcept;
-    bool validationLayersSupported() const noexcept;
-    void displayExtensionsInfo(std::vector<const char*>) const noexcept;
-
-    void initDebugMessenger() noexcept;
-    void setDebugMessengerCreateInfo(vk::DebugUtilsMessengerCreateInfoEXT&) const noexcept;
-
-    void initSurface();
-
-    void selectPhysicalDevice();
-    bool deviceIsSupported(const vk::PhysicalDevice&) noexcept;
-    void setQueueFamilies(const vk::PhysicalDevice&) noexcept;
-    bool deviceExtensionsSupported(const vk::PhysicalDevice&) const noexcept;
-    void setSwapChainDetails(const vk::PhysicalDevice&) noexcept;
-
-    void initLogicalDevice();
-
-    void initSwapChain();
-    vk::Extent2D getSwapExtent() const noexcept;
-    vk::SurfaceFormatKHR getSwapSurfaceFormat() const noexcept;
-    vk::PresentModeKHR getSwapPresentMode() const noexcept;
-
-    void initImageViews();
-
-    void initRenderPass();
-
-    void initPipeline();
-
-    void initFramebuffers();
-
-    void initCommandPool();
-    void initCommandBuffer();
-    void recordCommandBuffer(const vk::CommandBuffer&, uint32_t imageIndex);
-
-    void initSyncObjects();
-    void destroySyncObjects();
-
-    vk::Instance vkInst;
-    vk::SurfaceKHR surface;
-    vk::PhysicalDevice physicalDevice = nullptr;  // destroyed with VkInstance
-    vk::Device device;
-    vk::Queue graphicQueue;
-    vk::Queue presentQueue;
-    vk::SwapchainKHR swapChain;
-    std::vector<vk::Image> swapChainImages;
-    std::vector<vk::ImageView> swapChainImageViews;
-    vk::Format swapChainImageFormat;
-    vk::Extent2D swapChainExtent;
-    vk::RenderPass renderPass;
-    vk::PipelineLayout pipelineLayout;
-    vk::Pipeline pipeline;
-
-    std::vector<vk::Framebuffer> swapChainFramebuffers;
-    vk::Viewport viewport;
-
-    vk::CommandPool commandPool;
-    vk::CommandBuffer commandBuffer;
+    dot::Instance inst;
 
     Window& wnd;
-
-    vk::Semaphore imageAvailableSemaphore;
-    vk::Semaphore renderFinishedSemaphore;
-    vk::Fence inFlightFence;
-
-    const std::vector<const char*> deviceExtensions =
-    {
-        VK_KHR_SWAPCHAIN_EXTENSION_NAME
-    };
-    struct QueueFamilyIndices
-    {
-        std::optional<uint32_t> graphicFamily;
-        std::optional<uint32_t> presentFamily;
-
-        bool found() const noexcept
-        {
-            return
-                graphicFamily.has_value() &&
-                presentFamily.has_value();
-        }
-    } queueIndices;
-    struct SwapChainSupportDetails
-    {
-        vk::SurfaceCapabilitiesKHR capabilities;
-        std::vector<vk::SurfaceFormatKHR> formats;
-        std::vector<vk::PresentModeKHR> modes;
-    } swapChainDetails;
-
-    vk::DispatchLoaderDynamic dldi;
-    vk::DebugUtilsMessengerEXT debugMessenger;
-    const std::vector<const char*> validationLayers =
-    {
-        "VK_LAYER_KHRONOS_validation"
-    };
-    #ifdef NDEBUG
-        const bool validationLayersEnabled = false;
-    #else
-        const bool validationLayersEnabled = true;
-    #endif
 };
---
+++ engine/include/dot_Instance.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "dot_Vulkan.h"
+
+#include <vector>
+
+namespace dot
+{
+    class Instance
+    {
+    // public:
+    //     Instance();
+    //     ~Instance();
+    // private:
+    //     std::vector<const char*> getRequiredExtensions() const noexcept;
+    //     bool validationLayersSupported() const noexcept;
+    //     void displayExtensionsInfo(std::vector<const char*>) const noexcept;
+    //     void setDebugMessengerCreateInfo(vk::DebugUtilsMessengerCreateInfoEXT&) const noexcept;
+
+    //     vk::DispatchLoaderDynamic dldi;
+    //     #ifdef NDEBUG
+    //         const bool validationLayersEnabled = false;
+    //     #else
+    //         const bool validationLayersEnabled = true;
+    //     #endif
+    //     vk::DebugUtilsMessengerEXT debugMessenger;
+    //     const std::vector<const char*> validationLayers =
+    //     {
+    //         "VK_LAYER_KHRONOS_validation"
+    //     };
+    };
+}
