813d0151f5026c558195f19e6a328947e2f18304
fxkfjla <kacper.skr11@gmail.com>
2022-08-16 14:10:08 +0200

    create logical device

--- engine/include/dot_Device.h
+++ engine/include/dot_Device.h
@@ -5,6 +5,8 @@
 
 #include "Window.h"
 
+#include <optional>
+
 namespace dot
 {
     class Device
@@ -18,16 +20,43 @@ namespace dot
         ~Device();
     private:
         void createSurface();
-        // void selectPhysicalDevice();
-        // bool deviceSupported();
-        // void setQueueFamilies();
-        // bool deviceExtensionsSupported();
+
+        void selectPhysicalDevice();
+        bool deviceSupported(const vk::PhysicalDevice&);
+        void setQueueFamilies(const vk::PhysicalDevice&) noexcept;
+        bool deviceExtensionsSupported(const vk::PhysicalDevice&) const;
+        void setSwapchainDetails(const vk::PhysicalDevice&) noexcept;
+
+        void createLogicalDevice();
 
         vk::SurfaceKHR surface;
         vk::PhysicalDevice physicalDevice;
         vk::Device device;
-        vk::Queue graphicsQueue;
+        vk::Queue graphicQueue;
         vk::Queue presentQueue;
+
+        struct SwapChainSupportDetails
+        {
+            vk::SurfaceCapabilitiesKHR capabilities;
+            std::vector<vk::SurfaceFormatKHR> formats;
+            std::vector<vk::PresentModeKHR> modes;
+        } swapchainDetails;
+        struct QueueFamilyIndices
+        {
+            std::optional<uint32_t> graphicFamily;
+            std::optional<uint32_t> presentFamily;
+
+            bool found() const noexcept
+            {
+                return
+                    graphicFamily.has_value() &&
+                    presentFamily.has_value();
+            }
+        } queueIndices;
+        const std::vector<const char*> deviceExtensions =
+        {
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME
+        };
 
         dot::Instance inst;
         Window& wnd;
--- engine/src/dot_Device.cpp
+++ engine/src/dot_Device.cpp
@@ -1,16 +1,22 @@
 #include "dot_Device.h"
 #include "dot_Exception.h"
 
+#include <set>
+#include <string>
+
 namespace dot
 {
     Device::Device(Window& wnd)
         : wnd(wnd)
     {
         createSurface();
+        selectPhysicalDevice();
+        createLogicalDevice();
     }
 
     Device::~Device()
     {
+        device.destroy();
         inst.getVkInstance().destroySurfaceKHR(surface);
     }
 
@@ -22,5 +28,143 @@ namespace dot
             throw DOT_RUNTIME("Failed to create window surface!");
 
         surface = vk::SurfaceKHR(vkSurface);
+    }
+
+    void Device::selectPhysicalDevice()
+    {
+        try
+        {
+            std::vector<vk::PhysicalDevice> devices = inst.getVkInstance().enumeratePhysicalDevices();
+
+            for(const auto& device : devices)
+            {
+                if(deviceSupported(device))
+                {
+                    physicalDevice = device;
+                    break;
+                }
+            }
+        }
+        catch(const std::runtime_error& e)
+        {
+            throw DOT_RUNTIME_WHAT(e);
+        }
+
+        if(!physicalDevice)
+            throw DOT_RUNTIME("GPU not supported!");
+    }
+
+    bool Device::deviceSupported(const vk::PhysicalDevice& device)
+    {
+        setQueueFamilies(device);
+
+        bool extensionsSupported = deviceExtensionsSupported(device);
+        bool swapChainCompatible;
+        if(extensionsSupported)
+        {
+            setSwapchainDetails(device);
+            swapChainCompatible = !swapchainDetails.formats.empty() && !swapchainDetails.modes.empty();
+        }
+
+        return queueIndices.found() && extensionsSupported && swapChainCompatible;
+    }
+
+    void Device::setQueueFamilies(const vk::PhysicalDevice& device) noexcept
+    {
+        std::vector<vk::QueueFamilyProperties> queueFamilies = device.getQueueFamilyProperties();
+
+        vk::Bool32 presentSupport = VK_FALSE;
+        uint32_t i = 0;
+        for(const auto& queueFamily : queueFamilies)
+        {
+            if(queueFamily.queueFlags & vk::QueueFlagBits::eGraphics)
+                queueIndices.graphicFamily = i;
+            
+            if(device.getSurfaceSupportKHR(i, surface))
+                queueIndices.presentFamily = i;
+
+            if(queueIndices.found())
+                break;
+
+            i++;
+        }
+    }
+
+    bool Device::deviceExtensionsSupported(const vk::PhysicalDevice& device) const
+    {
+        try
+        {
+            std::vector<vk::ExtensionProperties> availableExtensions = device.enumerateDeviceExtensionProperties();
+
+            std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+
+            for(const auto& extension : availableExtensions)
+                requiredExtensions.erase(extension.extensionName);
+
+            return requiredExtensions.empty();
+        }
+        catch(const std::runtime_error& e)
+        {
+            throw DOT_RUNTIME_WHAT(e);
+        }
+    }
+
+    void Device::setSwapchainDetails(const vk::PhysicalDevice& device) noexcept
+    {
+
+        swapchainDetails.capabilities = device.getSurfaceCapabilitiesKHR(surface);
+        swapchainDetails.formats = device.getSurfaceFormatsKHR(surface);
+        swapchainDetails.modes = device.getSurfacePresentModesKHR(surface);
+    }
+
+    void Device::createLogicalDevice()
+    {
+        std::set<uint32_t> uniqueQueues = {queueIndices.graphicFamily.value(), queueIndices.presentFamily.value()};
+        std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos;
+
+        float queuePriorities[] = {1.0f};
+        for(auto queue : uniqueQueues)
+        {
+            vk::DeviceQueueCreateInfo queueCreateInfo
+            (
+                vk::DeviceQueueCreateFlags(0U),     // flags
+                queue,                              // queueFamilyIndex
+                queuePriorities                     // pQueuePriorities
+            );
+
+            queueCreateInfos.emplace_back(queueCreateInfo);
+        }
+
+        vk::PhysicalDeviceFeatures deviceFeatures = {};
+
+        auto validationLayers = inst.getValidationLayers();
+
+        vk::DeviceCreateInfo deviceCreateInfo
+        (
+            vk::DeviceCreateFlags(0U),  // flags
+            queueCreateInfos            // pQueueCreateInfos
+        );
+        deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
+        deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
+
+        if(inst.validationLayersEnabled())
+        {
+            deviceCreateInfo.enabledLayerCount = validationLayers.size();
+            deviceCreateInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+            deviceCreateInfo.enabledLayerCount = 0;
+        
+        try
+        {
+            device = physicalDevice.createDevice(deviceCreateInfo);
+        }
+        catch(const std::runtime_error& e)
+        {
+            throw DOT_RUNTIME_WHAT(e);
+        }
+        
+        graphicQueue = device.getQueue(0, queueIndices.graphicFamily.value());
+        presentQueue = device.getQueue(0, queueIndices.presentFamily.value());
     }
 }
