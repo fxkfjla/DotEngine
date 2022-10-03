030ff0d1ebd597b5e8444574b049f1ce14aba073
fxkfjla <kacper.skr11@gmail.com>
2022-10-03 15:31:26 +0200

    create vertex buffer for model

--- engine/include/dot_Model.h
+++ engine/include/dot_Model.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "dot_Vulkan.h"
+#include "dot_Device.h"
+#include "dot_Buffer.h"
 
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <memory>
 
 namespace dot
 {
@@ -19,12 +21,13 @@ namespace dot
             static std::vector<vk::VertexInputBindingDescription> getBindingDescription() noexcept;
             static std::vector<vk::VertexInputAttributeDescription> getAttributeDescription() noexcept;
         };
+        Model(Device&, const std::vector<Vertex>&);
+        void bind(const vk::CommandBuffer&) const noexcept;
     private:
-        std::vector<Vertex> verticies =
-        {
-            {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-            {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-        };
+        void createVertexBuffer(const std::vector<Vertex>&) noexcept;
+        std::unique_ptr<Buffer> vertexBuffer;
+        uint32_t vertexCount;
+
+        Device& device;
     };
 }
--- engine/src/dot_Model.cpp
+++ engine/src/dot_Model.cpp
@@ -1,10 +1,16 @@
 #include "dot_Model.h"
+#include "dot_Exception.h"
 
 namespace dot
 {
     std::vector<vk::VertexInputBindingDescription> Model::Vertex::getBindingDescription() noexcept
     {
-        return std::vector<vk::VertexInputBindingDescription>(0U, sizeof(dot::Model::Vertex));
+        std::vector<vk::VertexInputBindingDescription> bindingDescriptions;
+        bindingDescriptions.reserve(1);
+
+        bindingDescriptions.emplace_back(vk::VertexInputBindingDescription(0, sizeof(Model::Vertex)));
+
+        return bindingDescriptions;
     }
 
     std::vector<vk::VertexInputAttributeDescription> Model::Vertex::getAttributeDescription() noexcept
@@ -16,5 +22,32 @@ namespace dot
         attributeDescriptions.emplace_back(vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)));
 
         return attributeDescriptions;
+    }
+
+    Model::Model(Device& device, const std::vector<Vertex>& verticies) 
+        : device(device)
+    {
+        createVertexBuffer(verticies);
+    }
+
+    void Model::createVertexBuffer(const std::vector<Vertex>& verticies) noexcept
+    {
+        uint32_t vertexSize = sizeof(Vertex);
+        vertexCount = static_cast<uint32_t>(verticies.size());
+        vertexBuffer = std::make_unique<Buffer>
+        (
+            device, 
+            vertexSize, vertexCount, 
+            vk::BufferUsageFlagBits::eVertexBuffer,
+            vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
+        );
+
+        vertexBuffer->map();
+        vertexBuffer->write((void*)verticies.data());
+    } 
+
+    void Model::bind(const vk::CommandBuffer& cmdBuffer) const noexcept
+    {
+        cmdBuffer.bindVertexBuffers(0, vertexBuffer->getVkBuffer(), {0});
     }
 }
